53ceb8dfa9edabd5d9d2f145550188ae0d954fc1
Akhil <akhilsk555@gmail.com>
2022-02-10 12:30:29 +0530

    Issue Resolved

--- compute.h
+++
@@ -1,6 +0,0 @@
-#include "complex.h";
-
-complex_t complex_sum(complex_t C1, complex_t C2);
-complex_t complex_difference(complex_t C1, complex_t C2);
-complex_t complex_multiply(complex_t C1, complex_t C2);
-complex_t complex_divide(complex_t C1, complex_t C2);
